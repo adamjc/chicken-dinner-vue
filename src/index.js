a847ed0532806d09ceb1abd43c5fa3e50c62c167
adamjc <adamjc86@gmail.com>
2020-05-07 07:26:55 +0100

    Add initial actions

--- src/index.js
+++ src/index.js
@@ -1,22 +1,51 @@
 const Vue = require('vue')
 const { Engine: blackjack, STATES } = require('chicken-dinero')
 
-let game = blackjack()
+let game = new blackjack()
 
-function genActions (actions) {
+let id = setInterval(() => {
+  console.log('doin a step')
+  game.step()
 
-}
+  app.actions = game.getActions()
+  app.dealerHand = game.getDealerHand()
+  app.hand = game.getPlayer().hand
+}, 2000)
 
 let app = new Vue({
   el: '#app',
   data: {
     chips: game.getPlayer().chips,
-    wageredAmount: 0,
-    actions: game.getActions()
+    wageredAmount: 10,
+    actions: game.getActions(),
+    dealerHand: game.getDealerHand(),
+    hand: game.getPlayer().hand
   },
   methods: {
     wager: function () {
-      console.log('making a wager of', this.wageredAmount)
+      this.actions.wager(this.wageredAmount)
+      game.stepUntilChange() // Deal Player
+      game.stepUntilChange() // Deal Dealer
+
+      this.actions = game.getActions()
+      this.dealerHand = game.getDealerHand()
+      this.hand = game.getPlayer().hand
+    },
+    hit: function () {
+      this.actions.hit()
+      game.stepUntilChange()
+
+      this.actions = game.getActions()
+      this.dealerHand = game.getDealerHand()
+      this.hand = game.getPlayer().hand
+    },
+    stand: function () {
+      this.actions.stand()
+      game.stepUntilChange()
+
+      this.actions = game.getActions()
+      this.dealerHand = game.getDealerHand()
+      this.hand = game.getPlayer().hand
     }
   }
 })
